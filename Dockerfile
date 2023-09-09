4a57cd16658758973f9544998b07d94b0eecd004
ArielFerreiro <ariel.ferreiro@gmail.com>
2023-09-09 17:56:02 -0300

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,12 @@
+#docker build -t rest-api-flask-python .
+#docker run -dp 5000:5000 -w /app -v "$(pwd):/app" rest-api-flask-python
+
+FROM python:3.10
+EXPOSE 5000
+
+WORKDIR /app
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+COPY . .
+CMD ["flask", "run", "--host", "0.0.0.0"]
+
---
+++ app.py
@@ -0,0 +1,102 @@
+import os
+from flask import Flask, jsonify
+from flask_smorest import Api
+from resources.item import blp as ItemBluePrint
+from resources.store import blp as StoreBluePrint
+from resources.tag import blp as TagBluePrint
+from resources.user import blp as UserBluePrint
+
+from db import db
+from flask_jwt_extended import JWTManager
+from blocklist import BLOCKLIST
+from flask_migrate import Migrate
+
+def create_app(db_url=None):
+    app = Flask(__name__)
+
+    app.config["PROPAGATE_EXCEPTIONS"] = True
+    app.config["API_TITLE"] = "Stores REST API"
+    app.config["API_VERSION"] = "V1"
+    app.config["OPENAPI_VERSION"] = "3.0.3"
+    app.config["OPENAPI_URL_PREFIX"] = "/"
+    app.config["OPENAPI_SWAGGER_UI_PATH"] = "/swagger-ui"
+    app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
+    app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.init_app(app)
+    Migrate(app, db)
+    api = Api(app)
+    app.config["SWT_SECRET_KEY"] = "afdiaz75" #secrets.SystemRandom().getrandbits(128)
+    jwt = JWTManager(app)
+
+    @jwt.additional_claims_loader
+    def add_claims_to_jwt(identity):
+        # TODO: Read from a config file instead of hard-coding
+        # TODO: Esto esta viendo si el usuario es id 1 entonces es admin. Esta hardcodeado
+        if identity == 1:
+            return {"is_admin": True}
+        return {"is_admin": False}
+
+    @jwt.token_in_blocklist_loader
+    def check_if_token_in_blocklist(jwt_header, jwt_payload):
+        return jwt_payload["jti"] in BLOCKLIST
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
+
+    @jwt.needs_fresh_token_loader
+    def token_not_fresh_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {
+                    "description": "The token is not fresh.",
+                    "error": "fresh_token_required",
+                }
+            ),
+            401,
+        )
+
+    @jwt.revoked_token_loader
+    def revoked_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify(
+                {"description": "The token has been revoked.", "error": "token_revoked"}
+            ),
+            401,
+        )
+
+
+    with app.app_context():
+        db.create_all()
+
+    api.register_blueprint(ItemBluePrint)
+    api.register_blueprint(StoreBluePrint)
+    api.register_blueprint(TagBluePrint)
+    api.register_blueprint(UserBluePrint)
+
+    return app
